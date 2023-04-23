4fc973db04e4bf9dae08b2d2d636693fda3d6125
lfgonzami <123257425+lfgonzami@users.noreply.github.com>
2023-04-22 22:55:47 -0600

    Create Form1.cs

---
+++ Laboratorio Semana 8/Form1.cs
@@ -0,0 +1,134 @@
+namespace Lab8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // AGREGAR VALORES A LA COMBO BOX
+            cmbSeleccion.Items.Add("Sumatoria");
+            cmbSeleccion.Items.Add("Tablas de multiplicar");
+            cmbSeleccion.Items.Add("N�mero Perfecto");
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbSeleccion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            switch(cmbSeleccion.SelectedIndex)
+            {
+
+                case 0:
+                    tctSeleccion.SelectedTab = tabSumatoria; //MUESTRA LA PESTA�A DE SUMATORIA
+                    break;
+                case 1:
+                    tctSeleccion.SelectedTab = tabTablas; // MUESTA LA PESTA�A DE TABLAS
+                    break;
+                case 2:
+                    tctSeleccion.SelectedTab = tabNPerfecto; // MUESTRA LA PESTA�A DE N�MERO PERFECTO
+                    break;
+
+                default:
+                    break;
+
+            }
+
+            
+
+
+          
+
+        }
+
+        private void txtbIngreseN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+
+
+            if (tctSeleccion.SelectedTab == tabSumatoria)
+            {
+                int n = int.Parse(txtbIngreseN.Text);
+                int suma = 0;
+                int i = 1;
+
+                do
+                {
+                    suma += i;
+                    i++;
+                } while (i <= n);
+                LbResultadoN.Text = suma.ToString();
+            }
+            else if (tctSeleccion.SelectedTab == tabTablas)
+            {
+
+                string tabla = "  ";
+                for (int i = 1; i <= 10; i++)
+                {
+                    tabla += i + " ";
+                }
+                tabla += "\n";
+                for (int i = 1; i <= 10; i++)
+                {
+                    tabla += i + " ";
+                    for (int j = 1; j <= 10; j++)
+                    {
+                        tabla += (i * j) + " ";
+                    }
+                    tabla += "\n";
+                }
+
+                lbTabla.Text = tabla;
+
+
+            }
+            else if (tctSeleccion.SelectedTab == tabNPerfecto)
+            {
+
+
+                int num = Convert.ToInt32(txtbNPerfecto.Text);
+                int sum = 0;
+
+                for (int i = 1; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+
+                if (sum == num)
+                {
+                    lblResultadoPerfecto.Text = "El n�mero " + num.ToString() + " es perfecto.";
+                }
+                else
+                {
+                    lblResultadoPerfecto.Text = "El n�mero " + num.ToString() + " no es perfecto.";
+                }
+
+
+            }
+        
+
+            }
+
+        private void tabTablas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabFactorial_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
